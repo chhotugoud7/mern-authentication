8d1c6c93d0760778759bd084929893d46576c112
chhotugoud7 <chhotu957588@gmail.com>
2026-05-09 12:03:55 +0530

    initialize server with Express, CORS, and cookie-parser setup -- setup app with express

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors"; // used to connect backend and frontend 
+import 'dotenv/config';  // used to store involvement var in backend 
+import cookieParser from "cookie-parser"; 
+
+
+// making app 
+const app = express();
+const port = process.env.PORT || 4000
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credentials: true}))
+
+// {} object , ()=> arrow function 
+// `` backticks used to write template literals
+
+// checking backend server working on localhost:4000 port 
+app.get('/', (req, res)=> res.send("API Working fine"));
+
+//here whenever backend is started -- this app is started and this message is shown in terminal 
+app.listen(port, ()=> console.log(`Server started on PORT:${port}`));
+
+
